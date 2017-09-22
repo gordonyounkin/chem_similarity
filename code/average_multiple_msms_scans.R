6f9d459ccfd685ac89db5474a62ba6d7e250fa7e
Gordon Younkin <gordonyounkin@gmail.com>
2017-09-22 10:37:24 -0600

    Add extract.peakdata function

--- code/average_multiple_msms_scans.R
+++ code/average_multiple_msms_scans.R
@@ -1,12 +1,29 @@
 library(mzR)
 library(MSnbase)
 
-# Function finds weighted average of multiple msms scans. Requires MSnExp object containing MSMS files for species containing peak of interest as well as mz and rt of peak.
-avg.msms.spec <- function(data, rt, mz) {
-  rownames <- row.names(fData(ms2spec))[abs(fData(ms2spec)$retentionTime - rt*60) <= 20 & abs(fData(ms2spec)$precursorMZ - mz) <= 0.01]
-  spec1 <- lapply(1:length(rownames), function(j) as(data[[rownames[j]]], "data.frame"))
+# function that extracts mz/rt/TIC data for peaks from mzXML files. Requires vector containing full or relative filepaths for .mzXML files of interest and MSlevel to extract data for
+extract.peakdata <- function(files, MSlevel) {
+  curr_header <- header(openMSfile(files[1]))
+  msms_scans <- curr_header[curr_header$peaksCount >= 5 & curr_header$totIonCurrent >= 2000 & curr_header$basePeakIntensity >=1000 & curr_header$msLevel %in% MSlevel,]
+  if(nrow(msms_scans) > 0) msms_scans$file_idx <- 1
+  else msms_scans$file_idx <- numeric(0)
+  if(length(files) > 1) {
+  for(j in 2:length(files)) {
+    curr_header <- header(openMSfile(files[j]))
+    curr_header$file_idx <- j
+    msms_scans <- rbind(msms_scans, curr_header[curr_header$peaksCount >= 5 & curr_header$totIonCurrent >= 2000 & curr_header$basePeakIntensity >=1000 & curr_header$msLevel %in% MSlevel,])
+  }
+  }
+  return(msms_scans)
+}
+
+# Function finds weighted average of multiple msms scans. Requires msms_scans dataframe produced by extract.peakdata, vector containing file paths for MSMS .mzXML files (IN SAME ORDER AS USED FOR extract.peakdata function) as well as mz and rt of peak of interest.
+avg.msms.spec <- function(files, peak.data, rt, mz) {
+  scans_to_merge <- peak.data[abs(peak.data$retentionTime - rt*60) <= 20 & abs(peak.data$precursorMZ - mz) <= 0.01, c("acquisitionNum", "file_idx"), drop = FALSE]
+  if(nrow(scans_to_merge) < 1) return(NULL)
+  spec1 <- lapply(1:nrow(scans_to_merge), function(j) peaks(openMSfile(files[scans_to_merge$file_idx[j]]),scans_to_merge$acquisitionNum[j]))
   spec2 <- do.call(rbind, spec1)
-  spec3 <- spec2[order(spec2$i, decreasing = T),]
+  spec3 <- spec2[order(spec2[,2], decreasing = T),]
   row.names(spec3) <- 1:nrow(spec3)
   
   rows_done = numeric()
@@ -23,6 +40,7 @@ avg.msms.spec <- function(data, rt, mz) {
     rows_done <- c(rows_done, to_merge)
   }
   spec5 <- spec4[order(spec4[,1]),]
-  spec5 <- spec5[spec5[,2] >= 500,,drop=FALSE]
+  TICsum <- sum(spec5[,2])
+  spec5 <- spec5[spec5[,2] >= TICsum/1000,,drop=FALSE]
   return(spec5)
 }
