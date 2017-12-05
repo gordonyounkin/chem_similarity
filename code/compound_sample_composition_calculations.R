5302577de4ffcc227111e7be1549f24cac65e7ea
Gordon Younkin <gordonyounkin@gmail.com>
2017-12-05 16:48:32 -0700

    Code for minor analyses

---
+++ code/compound_sample_composition_calculations.R
@@ -0,0 +1,107 @@
+source("./code/create_pairwiseComps_sampsByComps.R")
+source("./code/chem_similarity_function.R")
+
+filled_comps <- read.csv("./data/filled_compound_table_2017_11_20.csv", stringsAsFactors=FALSE)
+filled_comps <- read.csv("./data/filled_compound_table_major_features_2017_11_30.csv", stringsAsFactors=FALSE)
+head(filled_comps)
+table(filled_comps$compound_sample)
+range(table(filled_comps$compound_sample))
+table(filled_comps$compound_sample)[table(filled_comps$compound_sample)==505]
+mean(table(filled_comps$compound_sample))
+length(unique(filled_comps$compound_number))
+
+# calculate relative abundance of compounds within each sample
+filled_comps$relTIC <- numeric(nrow(filled_comps))
+for(i in 1:length(unique(filled_comps$compound_sample))) {
+  sample <- unique(filled_comps$compound_sample)[i]
+  temp <- filled_comps[filled_comps$compound_sample == sample, ]
+  maxtic <- max(temp$TIC)
+  for(j in 1:nrow(temp)) {
+    filled_comps[filled_comps$compound_sample == sample & filled_comps$compound_number == temp$compound_number[j], "relTIC"] <- temp$TIC[j] / maxtic
+  }
+}
+filled_comps_trim <- filled_comps[filled_comps$relTIC > 0.001,]
+table(filled_comps_trim$compound_sample)
+range(table(filled_comps_trim$compound_sample))
+table(filled_comps_trim$compound_sample)[table(filled_comps_trim$compound_sample)==32]
+mean(table(filled_comps_trim$compound_sample))
+length(unique(filled_comps_trim$compound_number))
+
+# do species tend to share a lot of compounds?
+filled_comps_species <- filled_comps[startsWith(filled_comps$compound_sample, "LA43"),]
+
+
+hist(filled_comps_species$relTIC)
+
+table(filled_comps_species$compound_sample)
+table(filled_comps_trim$compound_sample)
+
+length(table(filled_comps_species$compound_number)[table(filled_comps_species$compound_number) %in% c(4,5)])
+sum(filled_comps_species[filled_comps_species$compound_number %in% as.numeric(names(table(filled_comps_species$compound_number)[table(filled_comps_species$compound_number) %in% c(1)])), "TIC"])
+hist(log(filled_comps_species[filled_comps_species$compound_number %in% as.numeric(names(table(filled_comps_species$compound_number)[table(filled_comps_species$compound_number) %in% c(1)])) & filled_comps_species$compound_sample == "LA43_1456", "TIC"]))
+
+
+filled_comps[filled_comps$compound_sample == "N34_1336",]
+
+hist(table(filled_comps$compound_number), breaks = 50, main = "With filled peaks", xlab = "Number of samples compound is found in")
+
+
+old_comp_table <- read.csv("./data/compound_tic_2017_08_02.csv")
+head(old_comp_table)
+table(old_comp_table$compound_sample)
+range(table(old_comp_table$compound_sample))
+mean(table(old_comp_table$compound_sample))
+table(old_comp_table$compound_sample)[table(old_comp_table$compound_sample)==224]
+
+hist(table(old_comp_table$compound_number), breaks = 50, main = "XCMS output", xlab = "Number of samples compound is found in")
+
+
+filled_sampsbycomps <- make_sampsByCompounds("./data/filled_compound_table_2017_11_20.csv",by_species = FALSE)
+filled_sampsbycomps_log <- log(filled_sampsbycomps)
+filled_sampsbycomps_log[filled_sampsbycomps_log<=0] <- 0
+filled_sampscompsstand <- standardizeByRow(filled_sampsbycomps)
+filled_sampsbycomps_log <- standardizeByRow(filled_sampsbycomps_log)
+filled_sampsbycomps_sqrt <- sqrt(filled_sampsbycomps)
+filled_sampcompsstand_sqrt <- standardizeByRow(filled_sampsbycomps_sqrt)
+filled_sampsbycomps_4rt <- filled_sampsbycomps**(1/4)
+filled_sampscompsstand_4rt <- standardizeByRow(filled_sampsbycomps_4rt)
+
+curr_sample <- unlist(filled_sampscompsstand[2,,drop=TRUE])
+curr_sample <- sort(curr_sample, decreasing = TRUE)
+plot(as.numeric(as.vector(curr_sample[curr_sample>0])) ~ as.numeric(1:sum(curr_sample>0)))
+
+# without any transformation
+row.names(filled_sampsbycomps)[504]
+curr_sample <- unlist(filled_sampscompsstand[504,,drop=TRUE])
+curr_sample <- sort(curr_sample, decreasing = TRUE)
+plot(as.numeric(as.vector(curr_sample[curr_sample>0])) ~ as.numeric(1:sum(curr_sample>0)))
+range(curr_sample[curr_sample>0])
+hist(curr_sample[curr_sample>0])
+
+# log of TIC
+curr_sample <- unlist(filled_sampsbycomps_log[504,,drop=TRUE])
+curr_sample <- sort(curr_sample, decreasing = TRUE)
+plot(as.numeric(as.vector(curr_sample[curr_sample>0])) ~ as.numeric(1:sum(curr_sample>0)))
+range(curr_sample[curr_sample>0])
+hist(curr_sample[curr_sample>0])
+
+# square root of TIC
+curr_sample <- unlist(filled_sampcompsstand_sqrt[504,,drop=TRUE])
+curr_sample <- sort(curr_sample, decreasing = TRUE)
+plot(as.numeric(as.vector(curr_sample[curr_sample>0])) ~ as.numeric(1:sum(curr_sample>0)))
+range(curr_sample[curr_sample>0])
+hist(curr_sample[curr_sample>0])
+
+# 4th root of TIC
+curr_sample <- unlist(filled_sampscompsstand_4rt[504,,drop=TRUE])
+curr_sample <- sort(curr_sample, decreasing = TRUE)
+plot(as.numeric(as.vector(curr_sample[curr_sample>0])) ~ as.numeric(1:sum(curr_sample>0)))
+range(curr_sample[curr_sample>0])
+hist(curr_sample[curr_sample>0])
+
+old_sampsbycomps <- make_sampsByCompounds("./data/compound_tic_2017_08_02.csv", by_species = FALSE)
+
+
+
+
+filled_sampsbycomps[1,]
--- code/create_boxplots_for_pairwise_sample_comparisons.R
+++ code/create_boxplots_for_pairwise_sample_comparisons.R
@@ -1,6 +1,6 @@
 library(RMySQL)
 
-chem_similarity <- read.csv("./results/combined_similarity_matrix_only_compclassavg_2017_11_20.csv")
+chem_similarity <- read.csv("./results/similarity_matrix_SQRT_sqrttocombine_2017_12_05.csv")
 chem_similarity <- chem_similarity[,names(chem_similarity) != "X"]
 row.names(chem_similarity) <- names(chem_similarity)
 
@@ -22,7 +22,7 @@ for(i in 1:nrow(spec_code_species)) {
   spec_code_species$species_name[i] <- species_name
 }
 table(unlist(spec_code_species$species_name))
-spec_code_species[spec_code_species$species_code == "M63", "species_name"] <- "umbellifera"
+spec_code_species[spec_code_species$species_code == "T05", "species_name"] <- "sapindoides"
 
 all_samps.df$species_1 <- unlist(sapply(1:nrow(all_samps.df), function(x) spec_code_species[spec_code_species$species_code == all_samps.df$spec_code_1[x], "species_name"]))
 all_samps.df$species_2 <- unlist(sapply(1:nrow(all_samps.df), function(x) spec_code_species[spec_code_species$species_code == all_samps.df$spec_code_2[x], "species_name"]))
@@ -41,21 +41,68 @@ for(i in 1:nrow(all_samps.df)) {
   }
 }
 
-dev.new()
+# throw out outliers??
+all_samps_trim <- all_samps.df[!startsWith(all_samps.df$X1, "LA20b_1745"),]
+all_samps_trim <- all_samps_trim[!startsWith(all_samps_trim$X2, "LA20b_1745"),]
+all_samps_trim <- all_samps_trim[!startsWith(all_samps_trim$X2, "LA9_696"),]
+all_samps_trim <- all_samps_trim[!startsWith(all_samps_trim$X1, "LA9_696"),]
+all_samps_trim <- all_samps_trim[!startsWith(all_samps_trim$X2, "N1_1250"),]
+all_samps_trim <- all_samps_trim[!startsWith(all_samps_trim$X1, "N1_1250"),]
+all_samps_trim <- all_samps_trim[!startsWith(all_samps_trim$X2, "T82_1291"),]
+all_samps_trim <- all_samps_trim[!startsWith(all_samps_trim$X1, "T82_1291"),]
+all_samps_trim <- all_samps_trim[!startsWith(all_samps_trim$X2, "LA43_669"),]
+all_samps_trim <- all_samps_trim[!startsWith(all_samps_trim$X1, "LA43_669"),]
+
+
+all_samps.df <- all_samps_trim
+#dev.new()
 boxplot(all_samps.df[all_samps.df$sample_pair_type == "all_others", "chemdist"],
   all_samps.df[all_samps.df$sample_pair_type == "same_spec_diff_site", "chemdist"],
   all_samps.df[all_samps.df$sample_pair_type == "same_spec_same_site", "chemdist"],
   names = c("all_others", "same species diff site", "same species/site"),
   main = "similarity calcs using ln(TIC)",
   ylab = "similarity score")
-dev.copy2pdf(file = "./results/sim_scores_by_sample_pair_type_LOGTIC_boxplot.pdf")
-dev.off()
+#dev.copy2pdf(file = "./results/sim_scores_by_sample_pair_type_LOGTIC_boxplot.pdf")
+#dev.off()
 
+hist(all_samps.df[all_samps.df$sample_pair_type == "all_others", "chemdist"])
+hist(all_samps.df[all_samps.df$sample_pair_type == "same_spec_diff_site", "chemdist"])
+hist(all_samps.df[all_samps.df$sample_pair_type == "same_spec_same_site", "chemdist"])
 
+all_samps.df_sqrtsqrt
+all_samps.df_sqrt
 all_samps.df_1mindiff 
 all_samps.df_mincompclass 
 all_samps.df_compclassavg 
 
-all_samps.df_1mindiff[all_samps.df_1mindiff$sample_pair_type == "same_spec_same_site" & all_samps.df_1mindiff$chemdis < 0.5, ]
+mean(all_samps.df_sqrtsqrt[all_samps.df_sqrtsqrt$sample_pair_type == "same_spec_same_site", "chemdist"])
+mean(all_samps.df_sqrt[all_samps.df_sqrt$sample_pair_type == "same_spec_same_site", "chemdist"])
+
+all_samps.df[all_samps.df$sample_pair_type == "same_spec_same_site" & all_samps.df$chemdist < 0.5, ]
+all_samps.df <- all_samps.df_mincompclass
 
 write.csv(all_samps.df[,names(all_samps.df) != "sample_pair_type"], "./results/all_samples_chem_scores_long_format.csv")
+
+# How much variation is there among chem of samples from same accession?
+head(all_samps.df)
+all_samps_same_species <- all_samps.df[all_samps.df$sample_pair_type == "same_spec_same_site",]
+species <- unique(all_samps_same_species$spec_code_1)
+species_variation <- data.frame("species" = species, "avg_score" = numeric(length(species)), "score_stdev" = numeric(length(species)))
+for(i in 1:length(species)) {
+  species_scores <- all_samps_same_species[all_samps_same_species$spec_code_1 == species[i], ]
+  species_variation$avg_score[i] <- mean(species_scores$chemdist)
+  species_variation$score_stdev[i] <- sd(species_scores$chemdist)
+}
+
+hist(species_variation$avg_score)
+hist(species_variation$score_stdev)
+
+species_variation[species_variation$avg_score > 0.87,]
+species_variation[species_variation$score_stdev > 0.2,]
+
+all_samps_same_species[all_samps_same_species$spec_code_1 == "N1", ]
+hist(all_samps_same_species[all_samps_same_species$spec_code_1 == "N1", "chemdist"])
+
+plot(species_variation$avg_score ~ species_variation$score_stdev)
+
+
