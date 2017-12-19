21cf295ea1d9916daf9046b83cb7bea3103157a1
Gordon Younkin <gordonyounkin@gmail.com>
2017-12-19 16:29:32 -0700

    fix run compound table code

--- code/run_compound_table_code.py
+++ code/run_compound_table_code.py
@@ -36,11 +36,11 @@ for row in result:
 feature_table = ct.build_feature_table(ftrd, mz_error=0.01, rt_error=0.3)
 
 # write csv of feature table
-with open("K://GY_LAB_FILES//github_repositories//chem_similarity//data//samples_with_polar_features_mzrt.csv", 'w') as file:
-    file.write("feature_number,sample,rt,mz,PC_ID,TIC\n")
+with open("K://GY_LAB_FILES//github_repositories//chem_similarity//data//samples_with_polar_features_mzrt.csv", 'w') as file1:
+    file1.write("feature_number,sample,rt,mz,PC_ID,TIC\n")
     for feature in feature_table:
         for sample in feature_table[feature]["sample"]:
-            file.write("%d,%s,%f,%f,%s,%f\n" % (feature, sample,\
+            file1.write("%d,%s,%f,%f,%s,%f\n" % (feature, sample,\
              feature_table[feature]["rt"][feature_table[feature]["sample"].index(sample)],\
              feature_table[feature]["mz"][feature_table[feature]["sample"].index(sample)],\
              feature_table[feature]["PC_IDs"][feature_table[feature]["sample"].index(sample)],\
@@ -51,17 +51,17 @@ pc_id_table = ct.build_pc_id(feature_table)
 compound_table = ct.build_compound_table(pc_id_table, min_cos_score=0.5)
 
 # write csv of compound table
-with open("K://GY_LAB_FILES//github_repositories//chem_similarity//data//polar_compound_feature_table.csv", "w") as file:
-    file.write("compound_number,feature_number,TIC,mz,rt\n")
+with open("K://GY_LAB_FILES//github_repositories//chem_similarity//data//polar_compound_feature_table.csv", "w") as file1:
+    file1.write("compound_number,feature_number,TIC,mz,rt\n")
     for compound in compound_table:
         for feature in compound_table[compound]["features"]:
-            file.write("%d,%d,%f,%f,%f\n" % (compound, feature, \
+            file1.write("%d,%d,%f,%f,%f\n" % (compound, feature, \
             compound_table[compound]["TICs"][compound_table[compound]["features"].index(feature)], \
             feature_table[feature]["avg_mz"], feature_table[feature]["avg_rt"]))
 
 # or read in compound table if it already exists:
-with open("K://GY_LAB_FILES//github_repositories//chem_similarity//data//polar_compound_feature_table.csv", "r") as file:
-    compound_table_temp = file.readlines()
+with open("K://GY_LAB_FILES//github_repositories//chem_similarity//data//polar_compound_feature_table.csv", "r") as file1:
+    compound_table_temp = file1.readlines()
 
 compound_table = {}
 for row in compound_table_temp:
@@ -84,8 +84,8 @@ for row in compound_table_temp:
             }
 
 # load filled features from R Code:
-with open("K://GY_LAB_FILES//github_repositories//chem_similarity//data//samples_with_polar_features_mzrt.csv", "r") as file:
-    filled_features_temp = file.readlines()
+with open("K://GY_LAB_FILES//github_repositories//chem_similarity//results//all_inga_filled_features_ppm_2017_12_05.csv", "r") as file1:
+    filled_features_temp = file1.readlines()
 
 filled_features = {}
 for row in filled_features_temp:
@@ -115,9 +115,9 @@ for row in filled_features_temp:
 filled_comps = ct.fill_compounds(filled_features, compound_table)
 
 # write csv of final compound table
-with open("K://GY_LAB_FILES//github_repositories//chem_similarity//data//polar_compound_table_2017_12_15.csv", "w") as file:
-    file.write("compound_sample,compound_number,TIC\n")
+with open("K://GY_LAB_FILES//github_repositories//chem_similarity//data//polar_compound_table_2017_12_15.csv", "w") as file1:
+    file1.write("compound_sample,compound_number,TIC\n")
     for sample in filled_comps:
         for i, compound in enumerate(filled_comps[sample]["compound"]):
-            file.write("%s,%d,%f\n" % (sample, compound, \
+            file1.write("%s,%d,%f\n" % (sample, compound, \
             filled_comps[sample]["TIC"][i]))
